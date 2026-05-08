e819ac196a09540b0b12c161eb588ac5c6f71980
wordpadonline <g98844836@gmail.com>
2026-05-09 00:45:22 +0500

    Update header.js

--- header.js
+++ header.js
@@ -35,7 +35,7 @@
 
           <!-- Header CTA -->
           <div class="header-cta">
-            <a href="#editor" class="btn btn-primary btn-sm">
+            <a href="/#editor" class="btn btn-primary btn-sm">
               <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                 <path d="M12 20h9"/>
                 <path d="M16.5 3.5a2.121 2.121 0 013 3L7 19l-4 1 1-4L16.5 3.5z"/>
